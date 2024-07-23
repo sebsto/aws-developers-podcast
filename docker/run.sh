78c0b9fe9b95b672f31c277130053f6d1fe47a21
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2024-07-23 14:29:09 +0200

    use local version of jekyll for local builds

--- docker/run.sh
+++ docker/run.sh
@@ -6,12 +6,13 @@
 # image from https://github.com/envygeeks/jekyll-docker
 
 # export JEKYLL_VERSION=3.8
-export JEKYLL_VERSION=4.0
-docker run \
-  -p 4000:4000 \
-  --rm \
-  --volume="$PWD:/srv/jekyll" \
-  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
-  -it jekyll/jekyll:$JEKYLL_VERSION \
-  jekyll serve -H 0.0.0.0 -P 4000 
-#   jekyll build --watch 
+# export JEKYLL_VERSION=4.0
+# docker run \
+#   -p 4000:4000 \
+#   --rm \
+#   --volume="$PWD:/srv/jekyll" \
+#   --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+#   -it jekyll/jekyll:$JEKYLL_VERSION \
+#   jekyll serve -H 0.0.0.0 -P 4000 
+
+ bundle exec jekyll serve 
