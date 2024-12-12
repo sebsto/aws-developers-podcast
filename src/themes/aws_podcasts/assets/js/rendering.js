45d1891f68f846766b905e23dc1cbdac660944b3
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2024-12-12 11:06:40 +0100

    format duration

--- src/themes/aws_podcasts/assets/js/rendering.js
+++ src/themes/aws_podcasts/assets/js/rendering.js
@@ -1,14 +1,43 @@
 function truncateText(text, n) {
-if (text.length <= n) {
-    return text;
+    if (text.length <= n) {
+        return text;
+    }
+    return text.substring(0, n) + '...';
 }
-return text.substring(0, n) + '...';
+
+function formatDuration(text) {
+    const timeParts = text.split(':');
+    const hours = parseInt(timeParts[0], 10);
+    const minutes = parseInt(timeParts[1], 10);
+    const seconds = parseInt(timeParts[2], 10);
+
+    let formattedDuration = '';
+    if (hours > 0) {
+        formattedDuration += `${hours} hr `;
+    }
+    if (minutes > 0) {
+        formattedDuration += `${minutes} min `;
+    }
+    if (seconds > 0) {
+        formattedDuration += `${seconds} secs`;
+    }
+    return formattedDuration.trim();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+
+// Truncate text
 const truncateElements = document.querySelectorAll('.truncate');
 truncateElements.forEach(function(element) {
-    const truncatedText = truncateText(element.textContent, 300); // Change 100 to the desired number of characters
+    const truncatedText = truncateText(element.textContent, 300); 
     element.textContent = truncatedText;
 });
+
+// Format duration
+const durationElements = document.querySelectorAll('.duration');
+durationElements.forEach(function(element) {
+    const durationText = formatDuration(element.textContent);
+    element.textContent = durationText;
+});
+
 });
